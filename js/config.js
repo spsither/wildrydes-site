4e5b7fac0fd000adcec49468a2296b76dcfc2813
Sonam Phuntsog <spsither@gmail.com>
2021-05-07 15:12:22 -0500

    cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_TkgnRtgTK', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1gr6vmf6btjo8up46ifahk2qth', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
